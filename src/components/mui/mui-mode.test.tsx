cdb8a4fc541098d497c1f08be20f0b4999dc53cb
themodernmonk7 <sauravavishek@gmail.com>
2022-09-19 19:49:13 +0530

    39-Custom Render Functions

--- src/components/mui/mui-mode.test.tsx
+++ src/components/mui/mui-mode.test.tsx
@@ -1,12 +1,9 @@
-import { render, screen } from "@testing-library/react"
-import { AppProviders } from "../../Providers/app-providers"
+import { render, screen } from "../../test-utils"
 import { MuiMode } from "./mui-mode"
 
 describe("MuiMode", () => {
   test("renders text correctly", () => {
-    render(<MuiMode />, {
-      wrapper: AppProviders,
-    })
+    render(<MuiMode />)
     const headingElement = screen.getByRole("heading")
     expect(headingElement).toHaveTextContent("dark")
   })
