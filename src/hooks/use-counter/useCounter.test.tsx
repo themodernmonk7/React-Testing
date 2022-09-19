be9a9ee82c0e3ae2958ee36dd9b238a0abd4872b
themodernmonk7 <sauravavishek@gmail.com>
2022-09-19 20:26:56 +0530

    41-Act Utility

--- src/hooks/use-counter/useCounter.test.tsx
+++ src/hooks/use-counter/useCounter.test.tsx
@@ -1,4 +1,5 @@
 import { renderHook } from "@testing-library/react"
+import { act } from "react-dom/test-utils"
 import { useCounter } from "./useCounter"
 
 describe("useCounter", () => {
@@ -14,5 +15,17 @@ describe("useCounter", () => {
       },
     })
     expect(result.current.count).toBe(10)
+  })
+
+  test("should increment the count", () => {
+    const { result } = renderHook(useCounter)
+    act(() => result.current.increment())
+    expect(result.current.count).toBe(1)
+  })
+
+  test("should decrement the count", () => {
+    const { result } = renderHook(useCounter)
+    act(() => result.current.decrement())
+    expect(result.current.count).toBe(-1)
   })
 })
